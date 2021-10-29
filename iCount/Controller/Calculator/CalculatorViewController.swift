f6b16c7fe897d1ca54e57654f3717a69ee8aa7db
Ionut Ciovica <ionut.ciovica@gmail.com>
2021-10-29 14:59:42 +0100

    Calculator 99% done!

--- iCount/Controller/Calculator/CalculatorViewController.swift
+++ iCount/Controller/Calculator/CalculatorViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     lazy var gradient: CAGradientLayer = {
          let gradient = CAGradientLayer()
          gradient.type = .axial
@@ -17,10 +19,24 @@ class CalculatorViewController: UIViewController {
              UIColor.purple.cgColor,
              UIColor.cyan.cgColor
          ]
-         gradient.locations = [0, 0.25, 1]
-         
+        gradient.locations = [0, 0.25, 1]
+
          return gradient
      }()
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +46,48 @@ class CalculatorViewController: UIViewController {
         gradient.startPoint = CGPoint(x: 0, y: 1)
         gradient.endPoint = CGPoint(x: 0, y: 0)
         view.layer.addSublayer(gradient)
+    }
+    
+    private var calculator = CalculatorLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+ 
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
 }
